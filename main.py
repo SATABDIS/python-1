c748053825a0c1a7bf5506dd639225ae470da3cd
SATABDIS <s.satabdi@gmail.com>
2019-12-01 16:04:56 +0000

    more statements

--- main.py
+++ main.py
@@ -3,4 +3,68 @@
 name=input ('What is your name?')
 print('hello   ' + name)
 
+#Fundamental data types
+
+#int and float data types
+
+print(type(3+6))
+
+print(type(3-6))
+
+print(type(3*6))
+
+print(type(3/6))
+
+print((3+6))
+
+
+print (2**3) #to thr power of
+print (3 // 4) # double divide
+print(5 // 4) # double divide
+print (5%4)  #modulo
+print(6%4)   #modulo
+
+# #math functions
+print(round(4.5))
+print(round(3.9))
+print(abs(-90))
+print(abs(90))
+
+#operator precedence
+
+print(20-3*4)
+print((20-3)+3**2)
+
+#bin data type
+print(bin(4))  # print the binary value 
+print(int('0b100',2))
+
+#use of variables
+a= 10
+
+print(a)
+
+# variables should start with lower case
+# no spaces should be used ,instead underscores can be used
+#Variables can comprise of letters,numbers and underscores
+#variables are case-sensitive
+
+#keywords are highlighted in blue in python and cannot be used as variables
+
+b=5
+c=a/b
+print(c)
+
+#constants
+#written in capitals
+#their value dosen't change
+
+PI= 3.14
+print(PI)
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
 
